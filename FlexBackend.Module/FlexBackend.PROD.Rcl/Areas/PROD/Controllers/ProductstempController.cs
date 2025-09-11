686c01dfedd0d53a0723401f232392b6dd61f5db
PeiXIn <qwop125445123@gmail.com>
2025-09-11 20:50:43 +0800

    20250911 更新框架頁面

---
+++ FlexBackend.Module/FlexBackend.PROD.Rcl/Areas/PROD/Controllers/ProductstempController.cs
@@ -0,0 +1,24 @@
+﻿using FlexBackend.Core.Interfaces.PROD;
+using FlexBackend.Infra.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FlexBackend.PROD.Rcl.Areas.PROD.Controllers
+{
+	[Area("PROD")]
+	public class ProductstempController : Controller
+	{
+
+		private readonly IProductService _repo;
+
+		public ProductstempController(IProductService repo)
+		{
+			_repo = repo;
+		}
+
+		public async Task<IActionResult> Index_ex_datatable()
+		{
+			var products = await _repo.GetAllAsync();
+			return View(products);
+		}
+	}
+}
