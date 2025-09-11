9c9a2c126c56e31b4b20c2a9d6fa36dfd3ff87d6
CCchien-pig <carterjean2001@gmail.com>
2025-09-11 09:56:06 +0800

    新建data存放UsersDbContext

---
+++ tHerdBackend.Module/FlexBackend.Users.Rcl/Data/UserDbContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace FlexBackend.USER.Rcl.Data
+{
+	public class UserDbContext : IdentityDbContext<IdentityUser>
+	{
+		public UserDbContext(DbContextOptions<UserDbContext> options)
+			: base(options)
+		{
+		}
+
+		// 你可以在這裡加入其他與使用者相關的 DbSet，例如會員資料、地址等
+		// public DbSet<MemberProfile> MemberProfiles { get; set; }
+
+		protected override void OnModelCreating(ModelBuilder builder)
+		{
+			base.OnModelCreating(builder);
+			// 可以在這裡設定資料庫模型，例如設定表名、屬性等
+		}
+	}
+}
