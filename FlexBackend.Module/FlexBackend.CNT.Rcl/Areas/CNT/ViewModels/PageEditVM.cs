ae3b065333004dd07f55453c19a1b62d55643e91
Mark <x222l99b1010e@gmail.com>
2025-09-11 15:54:54 +0800

    提交CNT版本

---
+++ FlexBackend.Module/FlexBackend.CNT.Rcl/Areas/CNT/ViewModels/PageEditVM.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlexBackend.CNT.Rcl.Areas.CNT.ViewModels
+{
+	internal class PageEditVM
+	{
+		//給 Create/Edit 用的
+	}
+}
