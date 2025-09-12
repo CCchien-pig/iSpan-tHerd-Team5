3a2ffa8fbca8b0711a4a974037b55ad1bb3706ec
Mark <x222l99b1010e@gmail.com>
2025-09-12 13:13:41 +0800

    更新CRUD

--- FlexBackend.Module/FlexBackend.CNT.Rcl/Areas/CNT/ViewModels/PageEditVM.cs
+++ FlexBackend.Module/FlexBackend.CNT.Rcl/Areas/CNT/ViewModels/PageEditVM.cs
@@ -6,8 +6,25 @@ using System.Threading.Tasks;
 
 namespace FlexBackend.CNT.Rcl.Areas.CNT.ViewModels
 {
-	internal class PageEditVM
+	public class PageEditVM
 	{
-		//給 Create/Edit 用的
+		public int PageId { get; set; }           // 文章 ID
+		public string Title { get; set; }         // 標題
+		public string Status { get; set; }        // 狀態 (資料庫是 varchar)
+
+		public DateTime? RevisedDate { get; set; } // 異動時間 (系統自動更新)
+
+
+		// 額外方便輸出的屬性
+		public string StatusText =>
+			Status switch
+			{
+				"0" => "草稿",
+				"1" => "已發佈",
+				"2" => "封存",
+				"9" => "刪除",
+				_ => "未知狀態"
+			};
 	}
 }
+
