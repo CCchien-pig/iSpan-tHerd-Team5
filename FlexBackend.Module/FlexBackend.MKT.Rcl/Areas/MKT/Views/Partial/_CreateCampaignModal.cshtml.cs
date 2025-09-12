8454e7cc654b1fac94dcd9ffe674b22595a634bd
Lihan <b0926920221@gmail.com>
2025-09-12 14:13:49 +0800

    2025/09/12 ( MKT ) 新增 [ 編輯活動 ] 和 [ 新增活動 ] 的局部檢視 刪除CreateCamaign.cshtml ( 用局部檢視代替 ) 完成活動的crud

--- FlexBackend.Module/FlexBackend.MKT.Rcl/Areas/MKT/Views/Partial/_CreateCampaignModal.cshtml.cs
+++ FlexBackend.Module/FlexBackend.MKT.Rcl/Areas/MKT/Views/Partial/_CreateCampaignModal.cshtml.cs
@@ -1,9 +1,9 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace FlexBackend.MKT.Rcl.Areas.MKT.Views.Campaigns
+namespace FlexBackend.MKT.Rcl.Areas.MKT.Views.Partial
 {
-    public class creatCampaignModel : PageModel
+    public class _CreateCampaignModalModel : PageModel
     {
         public void OnGet()
         {
