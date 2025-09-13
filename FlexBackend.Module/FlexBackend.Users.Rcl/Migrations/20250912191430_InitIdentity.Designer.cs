2af48de7648ddb794242525c52e6b7696ef06d79
CCchien-pig <carterjean2001@gmail.com>
2025-09-13 10:03:01 +0800

    IDENTITY資料庫更新完成

--- FlexBackend.Module/FlexBackend.Users.Rcl/Migrations/20250912191430_InitIdentity.Designer.cs
+++ FlexBackend.Module/FlexBackend.Users.Rcl/Migrations/20250912191430_InitIdentity.Designer.cs
@@ -12,8 +12,8 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace FlexBackend.USER.Rcl.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20250912123906_Default Schema")]
-    partial class DefaultSchema
+    [Migration("20250912191430_InitIdentity")]
+    partial class InitIdentity
     {
         /// <inheritdoc />
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
