70b46f9048aee3b46a21e69a9b3c076bd882d7c9
CCchien-pig <carterjean2001@gmail.com>
2025-09-14 22:23:13 +0800

    權限設定初步完成

---
+++ tHerdBackend.Module/FlexBackend.Users.Rcl/Areas/USER/Controllers/Members.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FlexBackend.USER.Rcl.Areas.USER.Controllers
+{
+	public class Members : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
