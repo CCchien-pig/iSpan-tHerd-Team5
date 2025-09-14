4a79c6693e8b397d94819b82726618c78b8d9bc5
CCchien-pig <carterjean2001@gmail.com>
2025-09-14 19:04:59 +0800

    0914-1904 identity重構成功

--- FlexBackend.Module/FlexBackend.Users.Rcl/Areas/Identity/Pages/Account/LoginWith2fa.cshtml.cs
+++ FlexBackend.Module/FlexBackend.Users.Rcl/Areas/Identity/Pages/Account/LoginWith2fa.cshtml.cs
@@ -9,7 +9,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
-using FlexBackend.USER.Rcl.Data;
+using FlexBackend.Core.DTOs.USER;
 using Microsoft.AspNetCore.Identity;
 
 namespace FlexBackend.USER.Rcl.Areas.Identity.Pages.Account
