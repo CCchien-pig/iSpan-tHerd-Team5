e42692e49e8e42756819845d125f57afc66f7f01
Nine9pLus <shih123584769@gmail.com>
2025-09-16 22:37:05 +0800

    123

---
+++ FlexBackend.Share/FlexBackend.Core/DTOs/USER/MeProfileVm.cs
@@ -0,0 +1,16 @@
+﻿namespace FlexBackend.Core.DTOs.USER
+{
+	public class MeProfileVm
+	{
+		public string Id { get; set; } = "";
+		public int UserNumberId { get; set; }   // ← 改這裡
+		public string? LastName { get; set; }
+		public string? FirstName { get; set; }
+		public string? Email { get; set; }
+		public string? PhoneNumber { get; set; }
+		public string Gender { get; set; } = "N/A";
+		public DateTime? BirthDate { get; set; }
+		public string? Address { get; set; }
+		public bool IsActive { get; set; }
+	}
+}
