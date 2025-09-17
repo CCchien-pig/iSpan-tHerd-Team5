85cd3e2dc90322a2510ca4a14da5a010ced76da3
Mark <x222l99b1010e@gmail.com>
2025-09-17 17:18:39 +0800

    必要優化及更新

---
+++ FlexBackend.Module/FlexBackend.CNT.Rcl/Areas/CNT/ViewModels/ScheduleListVM.cs
@@ -0,0 +1,23 @@
+﻿using FlexBackend.CNT.Rcl.Areas.CNT.ViewModels.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlexBackend.CNT.Rcl.Areas.CNT.ViewModels
+{
+	// ================================
+	// ViewModels
+	// ================================
+	public class ScheduleListVM
+	{
+		public int ScheduleId { get; set; }
+		public int PageId { get; set; }
+		public string PageTitle { get; set; }
+		public string PageTypeName { get; set; }
+		public ActionType ActionType { get; set; }
+		public DateTime ScheduledDate { get; set; }
+		public ScheduleStatus Status { get; set; }
+	}
+}
