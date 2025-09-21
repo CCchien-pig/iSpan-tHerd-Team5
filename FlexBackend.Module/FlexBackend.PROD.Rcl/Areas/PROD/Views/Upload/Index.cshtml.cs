85d1819acf614165946a5b00f412a750ce99d3ff
Clara-Yin <qwop125445123@gmail.com>
2025-09-22 03:04:07 +0800

    新增圖片上傳功能

---
+++ FlexBackend.Module/FlexBackend.PROD.Rcl/Areas/PROD/Views/Upload/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FlexBackend.PROD.Rcl.Areas.PROD.Views.Upload
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
