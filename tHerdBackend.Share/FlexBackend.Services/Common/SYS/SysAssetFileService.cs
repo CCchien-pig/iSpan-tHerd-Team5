2fa8a86d4d19927d71047f07d88bf9f832a31ed9
Clara-Yin <qwop125445123@gmail.com>
2025-09-24 01:52:55 +0800

    新增圖片讀取功能

---
+++ tHerdBackend.Share/FlexBackend.Services/Common/SYS/SysAssetFileService.cs
@@ -0,0 +1,25 @@
+﻿using FlexBackend.Core.Interfaces.SYS;
+using Microsoft.AspNetCore.Http;
+
+namespace FlexBackend.Services.PROD
+{
+    public class SysAssetFileService : ISysAssetFileService
+    {
+        private readonly ISysAssetFileRepository _frepo;
+
+        public SysAssetFileService(ISysAssetFileRepository frepo)
+        {
+            _frepo = frepo;
+        }
+
+        /// <summary>
+        /// 新增相片至 Cloudinary 並存入資料庫
+        /// </summary>
+        /// <param name="files"></param>
+        /// <returns></returns>
+        public Task<bool> AddImages(List<IFormFile> files)
+        {
+            return _frepo.AddImages(files);
+        }
+    }
+}
