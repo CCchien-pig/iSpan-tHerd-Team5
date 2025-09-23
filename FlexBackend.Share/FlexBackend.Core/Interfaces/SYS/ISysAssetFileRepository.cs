c5848217bbafb21348ce262a9a903507a05d1f94
Clara-Yin <qwop125445123@gmail.com>
2025-09-24 01:52:55 +0800

    新增圖片讀取功能

---
+++ FlexBackend.Share/FlexBackend.Core/Interfaces/SYS/ISysAssetFileRepository.cs
@@ -0,0 +1,10 @@
+﻿using FlexBackend.Core.DTOs;
+using Microsoft.AspNetCore.Http;
+
+namespace FlexBackend.Core.Interfaces.SYS
+{
+    public interface ISysAssetFileRepository
+    {
+        Task<bool> AddImages(List<IFormFile> files);
+    }
+}
