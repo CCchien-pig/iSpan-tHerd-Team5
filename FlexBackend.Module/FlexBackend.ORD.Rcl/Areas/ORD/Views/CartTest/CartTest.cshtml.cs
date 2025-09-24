39facff1da618d087bd8d0cbb84cb30af3c1f664
PINCHI <lynnmi.ispan@gmail.com>
2025-09-24 14:15:20 +0800

    拉出虛擬購物車頁面

---
+++ FlexBackend.Module/FlexBackend.ORD.Rcl/Areas/ORD/Views/CartTest/CartTest.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FlexBackend.ORD.Rcl.Areas.ORD.Views.CartTest
+{
+    public class CartTestModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
