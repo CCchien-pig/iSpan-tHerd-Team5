838f961150f021835c61a8e00c725e39fceb7bba
Lihan <b0926920221@gmail.com>
2025-09-27 15:31:33 +0800

    2025.09.27(MKT)修改刪除關聯資料庫提示訊息和新增優惠券種類

--- FlexBackend.Module/FlexBackend.MKT.Rcl/Areas/MKT/Controllers/CampaignsController.cs
+++ FlexBackend.Module/FlexBackend.MKT.Rcl/Areas/MKT/Controllers/CampaignsController.cs
@@ -1,7 +1,8 @@
 ﻿using FlexBackend.Infra.Models;
+using FlexBackend.MKT.Rcl.Areas.MKT.Utils;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using FlexBackend.MKT.Rcl.Areas.MKT.Utils;
+using System.Linq.Expressions;
 
 namespace FlexBackend.MKT.Rcl.Areas.MKT.Controllers
 {
@@ -124,10 +125,62 @@ namespace FlexBackend.MKT.Rcl.Areas.MKT.Controllers
             if (campaign == null)
                 return Json(new { success = false, message = "找不到活動" });
 
-            _context.MktCampaigns.Remove(campaign);
-            _context.SaveChanges();
-            return Json(new { success = true });
+            // 取得 Campaign 的 EntityType
+            var entityType = _context.Model.FindEntityType(typeof(MktCampaign));
+
+            // 找到所有指向 MktCampaign 的外鍵
+            var foreignKeys = _context.Model
+                .GetEntityTypes()
+                .SelectMany(et => et.GetForeignKeys())
+                .Where(fk => fk.PrincipalEntityType == entityType);
+
+            foreach (var fk in foreignKeys)
+            {
+                var dependentClrType = fk.DeclaringEntityType.ClrType;
+
+                // 利用反射呼叫 _context.Set<dependentClrType>()
+                var method = typeof(DbContext).GetMethod(nameof(DbContext.Set), Type.EmptyTypes);
+                var generic = method.MakeGenericMethod(dependentClrType);
+                var dbSet = (IQueryable)generic.Invoke(_context, null);
+
+                // 取外鍵屬性名稱 (假設只有一個 FK)
+                var fkProperty = fk.Properties.First();
+
+                // 動態建立 e => e.FK == id
+                var parameter = Expression.Parameter(dependentClrType, "e");
+                var property = Expression.Property(parameter, fkProperty.Name);
+                var constant = Expression.Constant(id);
+                var equal = Expression.Equal(property, constant);
+
+                var lambda = Expression.Lambda(equal, parameter);
+
+                // 呼叫 Any() 檢查有沒有關聯
+                var anyMethod = typeof(Queryable).GetMethods()
+                    .First(m => m.Name == "Any" && m.GetParameters().Length == 2)
+                    .MakeGenericMethod(dependentClrType);
+
+                bool hasRelation = (bool)anyMethod.Invoke(null, new object[] { dbSet, lambda });
+
+                if (hasRelation)
+                {
+                    return Json(new { success = false, message = "無法刪除已有關聯的活動" });
+                }
+            }
+
+            try
+            {
+                _context.MktCampaigns.Remove(campaign);
+                _context.SaveChanges();
+                return Json(new { success = true, message = "刪除成功" });
+            }
+            catch (Exception ex)
+            {
+                return Json(new { success = false, message = $"刪除失敗：{ex.Message}" });
+            }
         }
+
+
+
 
         // GET: 取得活動總數
         [HttpGet]
