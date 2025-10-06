ee60451a0d854814043e4b61e26c4b81ac8bd5d4
Nine9pLus <shih123584769@gmail.com>
2025-10-06 21:25:19 +0800

    apitest

---
+++ FlexBackend.Share/FlexBackend.Core/DTOs/SUP/LogisticsDTO.cs
@@ -0,0 +1,42 @@
+﻿namespace FlexBackend.SUP.Rcl.Areas.SUP.Controllers.ApiControllers
+{
+	public class LogisticsDTO
+	{
+		/// <summary>
+		/// 主鍵
+		/// </summary>
+		public int LogisticsId { get; set; }
+
+		public string ShippingMethod { get; set; }
+
+		/// <summary>
+		/// 物流商名稱
+		/// </summary>
+		public string LogisticsName { get; set; }
+
+		/// <summary>
+		/// 是否啟用
+		/// </summary>
+		public bool IsActive { get; set; }
+
+		/// <summary>
+		/// 建檔人員
+		/// </summary>
+		public int? Creator { get; set; }
+
+		/// <summary>
+		/// 建檔時間
+		/// </summary>
+		public DateTime CreatedDate { get; set; }
+
+		/// <summary>
+		/// 異動人員
+		/// </summary>
+		public int? Reviser { get; set; }
+
+		/// <summary>
+		/// 異動時間
+		/// </summary>
+		public DateTime? RevisedDate { get; set; }
+	}
+}
