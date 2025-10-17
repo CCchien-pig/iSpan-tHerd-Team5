78427539d899876593ef740063eeed887f26a3ce
x222l <x222l99b1010e@gmail.com>
2025-10-17 15:27:21 +0800

    新增文章頁面

--- tHerdBackend.Share/tHerdBackend.Services/CNT/CNTModuleServiceCollectionExtensions.cs
+++ tHerdBackend.Share/tHerdBackend.Services/CNT/CNTModuleServiceCollectionExtensions.cs
@@ -1,12 +1,18 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
+using tHerdBackend.Core.Interfaces.CNT;
+using tHerdBackend.Infra.Repository.CNT;
 
 namespace tHerdBackend.Services.CNT
 {
-    public static class CNTModuleServiceCollectionExtensions
-    {
-        public static IServiceCollection AddCNTModule(this IServiceCollection services)
-        {
-            return services;
-        }
-    }
+	public static class CNTModuleServiceCollectionExtensions
+	{
+		public static IServiceCollection AddCNTModule(this IServiceCollection services)
+		{
+			// 🟢 正確註冊 CNT 模組服務
+			services.AddScoped<IContentService, ContentService>();
+			services.AddScoped<ICntQueryRepository, CntQueryRepository>();
+
+			return services;
+		}
+	}
 }
